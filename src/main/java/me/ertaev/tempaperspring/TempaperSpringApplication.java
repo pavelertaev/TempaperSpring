731e1d3f7173b114fd5ee3f9b20c63205592b838
паша <pashka_ertaev@mail.ru>
2023-01-06 14:29:09 +0300

    Initial commit

---
+++ src/main/java/me/ertaev/tempaperspring/TempaperSpringApplication.java
@@ -0,0 +1,13 @@
+package me.ertaev.tempaperspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempaperSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempaperSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ertaev/tempaperspring/TempaperSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ertaev.tempaperspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempaperSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
